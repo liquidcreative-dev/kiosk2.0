758a987bd5100742b0d40dbf0889d956f99d50f1
liquidDev <Ledavid180@gmail.com>
2023-06-29 12:34:33 -0500

    db_name

--- backend/config/env/production/database.js
+++ backend/config/env/production/database.js
@@ -3,7 +3,8 @@ module.exports = ({ env }) => ({
         client: 'postgres',
         connection: {
             host: env('DATABASE_HOST'),
-            database: env('DATABASE_NAME', 5432),
+            port: env.int('DATABASE_PORT', 5432),
+            database: env('DATABASE_NAME'),
             user: env('DATABASE_USERNAME'),
             password: env('DATABASE_PASSWORD'),
             schema: env('DATABASE_SCHEMA'), // Not required
