c2c03224ab643ffc00087a675ce32f90609766a6
liquidDev <Ledavid180@gmail.com>
2023-06-29 15:43:53 -0500

    adding import-export-entries/changes to schema and config

--- backend/config/database.js
+++ backend/config/database.js
@@ -75,37 +75,37 @@
 
 
 
-// const path = require('path');
-
-// module.exports = ({ env }) => ({
-//   connection: {
-//     client: 'sqlite',
-//     connection: {
-//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-//     },
-//     useNullAsDefault: true,
-//   },
-// });
+const path = require('path');
 
 module.exports = ({ env }) => ({
   connection: {
-      client: 'postgres',
-      connection: {
-          host: env('DATABASE_HOST'),
-          port: env.int('DATABASE_PORT'),
-          database: env('DATABASE_NAME'),
-          user: env('DATABASE_USERNAME'),
-          password: env('DATABASE_PASSWORD'),
-          schema: env('DATABASE_SCHEMA'), // Not required
-          ssl: {
-              ca: env('DATABASE_CA')
-          },
-      },
-      pool: {
-          min: 0,
-          max: 1000,
-          acquireTimeoutMillis: 30000,  // increase this number as needed
-      },
-      debug: false,
+    client: 'sqlite',
+    connection: {
+      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+    },
+    useNullAsDefault: true,
   },
 });
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//       client: 'postgres',
+//       connection: {
+//           host: env('DATABASE_HOST'),
+//           port: env.int('DATABASE_PORT'),
+//           database: env('DATABASE_NAME'),
+//           user: env('DATABASE_USERNAME'),
+//           password: env('DATABASE_PASSWORD'),
+//           schema: env('DATABASE_SCHEMA'), // Not required
+//           ssl: {
+//               ca: env('DATABASE_CA')
+//           },
+//       },
+//       pool: {
+//           min: 0,
+//           max: 1000,
+//           acquireTimeoutMillis: 30000,  // increase this number as needed
+//       },
+//       debug: false,
+//   },
+// });
--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -15,4 +15,18 @@ module.exports = ({ env }) => ({
           },
         },
       },
+
+      'import-export-entries': {
+        enabled: true,
+        config: {
+          // See `Config` section.
+           /**
+       * Public hostname of the server.
+       *
+       * If you use the local provider to persist medias,
+       * `serverPublicHostname` should be set to properly export media urls.
+       */
+      serverPublicHostname: 'https://lobster-app-6xzrh.ondigitalocean.app/', // default: "".
+        },
+      },
 });
