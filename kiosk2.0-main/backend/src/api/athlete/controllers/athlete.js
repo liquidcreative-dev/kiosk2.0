d43501238050945df4ba366828fed763297c36df
liquidDev <Ledavid180@gmail.com>
2023-06-29 09:33:39 -0500

    Got rid of extra backup

--- kiosk2.0-main/backend/src/api/athlete/controllers/athlete.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * athlete controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::athlete.athlete');
