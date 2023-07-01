037f3ba003f2827339ee58b7d93b67c04c8d6aeb
liquidDev <Ledavid180@gmail.com>
2023-07-01 16:16:20 -0500

    testing middlewares

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -39,26 +39,7 @@
 //   'strapi::public',
 // ];
 module.exports = [
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com'],
-          'media-src': [
-            "'self'",
-            'data:',
-            'blob:',
-            'market-assets.strapi.io',
-            'res.cloudinary.com',
-          ],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
+  
   'strapi::errors',
   'strapi::security',
   'strapi::cors',
@@ -69,4 +50,24 @@ module.exports = [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ['self', 'http:'],
+          'img-src': ['self', 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com'],
+          'media-src': [
+            'self',
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'res.cloudinary.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
 ];
