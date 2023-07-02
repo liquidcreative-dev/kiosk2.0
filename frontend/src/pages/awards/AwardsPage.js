187bb37c0dd90c4bc9527149fb6f8c313a0fe3e4
liquidDev <Ledavid180@gmail.com>
2023-07-02 16:43:28 -0500

    AWARD PAGE

--- frontend/src/pages/awards/AwardsPage.js
+++ frontend/src/pages/awards/AwardsPage.js
@@ -23,26 +23,26 @@ const AwardsPage = () => {
 
 
   return (
-    <div className="container-fluid">
+    <div className="awardPage">
       {/* <div className="position-absolute bg-dark awardContainer"> */}
       <div className="bg-dark awardBackground">
         <MSHFWhiteLogo className="img-fluid whiteLogoAwards" />
         <div className="awards container-fluid">
-          <div className="row row-cols-1 row-cols-sm-3 row-cols-md-4 ">
+          <div className="row row-cols-1 row-cols-sm-3 row-cols-md-4">
             {awards.data && awards.data.length > 0 && awards.data.map((award) => (
               <div key={award.id} className="col">
                 <Link to={`/awards/${award.id}`} className="text-decoration-none">
                   <div className="award-banner bg-dark img-fluid background" style={{ backgroundImage: `url(${award?.attributes.selectImageLink})` }}>
-                    <div className="award-content">
+                    {/* <div className="award-content"> */}
 
                       <img src={award.attributes.iconLink} className="awardIcon" alt="award-icon" />
 
                       <div className="display-flex trophyNameContainer">
                         <div className="trophyName text-uppercase text-white">
-                        <p className="awardNameFull athleticFont">{award.attributes.name}</p><span className="firstNameText">Trophy</span>
+                          <p className="awardNameFull athleticFont">{award.attributes.name}</p><span className="firstNameText">Trophy</span>
                         </div>
                       </div>
-                    </div>
+                    {/* </div> */}
                   </div>
                 </Link>
               </div>
