94177845a450f3c9094f85a2c92e2390068345f3
Ledavid180 <38509457+Ledavid180@users.noreply.github.com>
2023-07-19 15:47:30 -0500

    Update Home.js
    Changed home photo

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import GirlAthlete from './GirlAthlete.jpg'
+import StartImg from './kioskOpenPic.png'
 import { ReactComponent as MSHFColorLogo } from './sports_hall_logo_color.svg';
 
 const Home = () => {
@@ -11,7 +11,7 @@ const Home = () => {
             <div className="row h-100 position-absolute w-100 homeBackground">
                 <div className="col-md-2 yellowBanner"></div>
                 <div className="col-md-10 p-0">
-                    <img src={GirlAthlete} className="img-fluid h-100 w-100" alt="Girl Athlete" />
+                    <img src={StartImg} className="img-fluid h-100 w-100" alt="Girl Athlete" />
                 </div>
             </div>
             <div className="row justify-content-center align-items-center text-center">
