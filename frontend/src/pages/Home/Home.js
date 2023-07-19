f20029e2115d4511629eb8d79ebdcd82ef7b2ce9
Ledavid180 <38509457+Ledavid180@users.noreply.github.com>
2023-07-19 17:03:28 -0500

    Update Home.js
    Trying to restore home page.

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -7,20 +7,16 @@ const Home = () => {
     const navigate = useNavigate();
 
     return (
+    <>
         <div className="container-fluid" style={{height: '100vh'}}>
-                    <img src={StartImg} className="img-fluid" alt="sports background" />
-                </div>
-            <div className="row justify-content-center align-items-center text-center">
-                <div className="text-white">
-                    // <MSHFColorLogo className="img-fluid colorLogo" />
-                    // <div >
-                    //     <span className="theWord">The</span>
-                    //     <span className=" text-uppercase homeText athleticFont">Outstanding</span>
-                    //     <h4 className="text-uppercase homeText athleticFont">Player Awards</h4>
-                    // </div>
-                    <button className="btn btnStyle btnHome athleticFont" onClick={() => navigate("/awards")}>Let's Go</button>
-                </div>
+            <img src={StartImg} className="img-fluid" alt="sports background" />
+        </div>
+        <div className="row justify-content-center align-items-center text-center">
+            <div className="text-white">
+                <button className="btn btnStyle btnHome athleticFont" onClick={() => navigate("/awards")}>Let's Go</button>
             </div>
+        </div>
+    </>
     );
 };
 
