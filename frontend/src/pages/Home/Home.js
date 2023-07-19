b0aaeccbb71382b3f5544dab4f742e92389dd6c2
Ledavid180 <38509457+Ledavid180@users.noreply.github.com>
2023-07-19 16:31:30 -0500

    Update Home.js
    More changes to home page to change background image.

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -7,13 +7,10 @@ const Home = () => {
     const navigate = useNavigate();
 
     return (
-        <div className="container-fluid position-relative" style={{height: '100vh'}}>
+        <div className="container-fluid" style={{height: '100vh'}}>
             <div className="row h-100 position-absolute w-100">
-                // <div className="col-md-2 yellowBanner"></div>
-                <div className="col-md-10 p-0">
-                    <img src={StartImg} className="img-fluid h-100 w-100" alt="Girl Athlete" />
+                    <img src={StartImg} className="img-fluid h-100 w-100" alt="sports background" />
                 </div>
-            </div>
             <div className="row justify-content-center align-items-center text-center">
                 <div className="text-white">
                     // <MSHFColorLogo className="img-fluid colorLogo" />
