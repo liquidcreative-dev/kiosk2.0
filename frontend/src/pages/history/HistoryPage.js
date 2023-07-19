5314035bee803551bccba5439f11fd42b9b62ad8
Ledavid180 <38509457+Ledavid180@users.noreply.github.com>
2023-07-19 18:29:28 -0500

    Update HistoryPage.js
    Fixing the same thing again.

--- frontend/src/pages/history/HistoryPage.js
+++ frontend/src/pages/history/HistoryPage.js
@@ -71,7 +71,7 @@ console.log(award);
           <div className="card text-white yearWinnerBanner">
             <div className="card-body d-flex justify-content-between align-items-center">
               <div>
-                <h1 className="yearBannerText athleticFont">{winner.award?.data?.attributes?.name}</h1>
+                <h1 className="yearBannerText athleticFont">{award.data.attributes.name}</h1>
               </div>
               <img src={award.data.attributes.sponsorLogoLink} alt="sponsor-logo" className="my-4 img-fluid sponsorLogo" style={{maxWidth: 'fit-content'}} />
             </div>
