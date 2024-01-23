9cae353a664850e82b7d0ca28d5626e0e04a878c
liquidDev <Ledavid180@gmail.com>
2024-01-23 10:35:06 -0600

    logic for less than 3 highlights

--- frontend/src/pages/highlights/HighlightModal.js
+++ frontend/src/pages/highlights/HighlightModal.js
@@ -11,6 +11,21 @@ const HighlightModal = ({ highlights, onClose }) => {
   const handleCloseVideo = () => {
     setCurrentVideo(null);
   };
+
+  // Function to get the container class based on the number of highlights
+  const getContainerClass = (index) => {
+    if (highlights.length === 1) {
+      // Center the single highlight
+      return "col-md-6 mx-auto";
+    } else if (highlights.length === 2) {
+      // Position two highlights at start and end
+      return index === 0 ? "col-md-6 start" : "col-md-6 end";
+    } else {
+      // Default case for three or more highlights
+      return "col-md-4";
+    }
+  };
+
   console.log(highlights);
   return (
     <div className="card highlight-component mb-3">
@@ -21,15 +36,15 @@ const HighlightModal = ({ highlights, onClose }) => {
             <h5 className="statsTitle text-center athleticFont">-HIGHLIGHTS-</h5>
             <div className="row historyScroll" style={{ overflowY: 'auto', maxHeight: '500px' }}>
               {highlights.map((highlight, index) => (
-                <div className="col-md-4 highlight-item mb-3" key={index} onClick={() => handleVideoClick(highlight.videoLink)}>
+                <div className={`${getContainerClass()} highlight-item mb-3`} key={index} onClick={() => handleVideoClick(highlight.videoLink)}>
                   <div className="d-flex justify-content-center align-items-center my-2">
                     <div className="col">
-                        <div className="row">
-                            <video className='img-fluid video-thumb' src={highlight.videoLink} alt="video" />
-                        </div>
-                        <div className="row">
-                            <p className="highlight-title text-capitalize">{highlight.name}</p>
-                        </div>
+                      <div className="row">
+                        <video className='img-fluid video-thumb' src={highlight.videoLink} alt="video" />
+                      </div>
+                      <div className="row">
+                        <p className="highlight-title text-capitalize">{highlight.name}</p>
+                      </div>
                     </div>
                   </div>
                 </div>
